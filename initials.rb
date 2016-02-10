0ff172a34488777c4e292043596a0016e69297c9
Matt Pindell <pindell.m@gmail.com>
2016-02-10 08:34:56 -0700

    desperate attempt at iter 2

--- initials.rb
+++ initials.rb
@@ -1,7 +1,21 @@
+require 'pry'
 
 # iteration 1
 initials = ARGV.map do |name|
   name[0].upcase
 end.join
 
-puts initials
+# iteration 2
+def cyphertext(char)
+  alphabet = ('a'..'z').to_a
+  orig_index = alphabet.find_index(char)
+  alphabet[orig_index + 2]
+end
+
+cipher = ARGV.map do |message|
+  message.chars.map do |char|
+    cyphertext(char)
+  end.join
+end.join(' ')
+
+puts cipher
