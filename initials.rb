251d656213fd8364853b7bdcf428b84e50e8ee77
Matt Pindell <pindell.m@gmail.com>
2016-02-10 09:00:51 -0700

    more updates

--- initials.rb
+++ initials.rb
@@ -6,15 +6,27 @@ initials = ARGV.map do |name|
 end.join
 
 # iteration 2
-def cyphertext(char)
+def cyphertext(char, key)
   alphabet = ('a'..'z').to_a
+  coded = alphabet.rotate(key)
   orig_index = alphabet.find_index(char)
-  alphabet[orig_index + 2]
+  rotate_method(orig_index, key, alphabet)
 end
 
-cipher = ARGV.map do |message|
+def rotate_method(orig_index, key, alphabet)
+  final_rotate = orig_index + key
+  if final_rotate > 26
+    final_rotate = final_rotate - 27
+  end
+  alphabet[final_rotate]
+end
+
+key = ARGV[0].to_i
+message = ARGV[1..-1]
+
+cipher = message.map do |message|
   message.chars.map do |char|
-    cyphertext(char)
+    cyphertext(char, key)
   end.join
 end.join(' ')
 
